ff7de4fe33448cd8893f9ebef48806f09f9e100b
Baatchia Riyadh <baatchia_riyadh@protonmail.com>
2019-06-15 01:56:58 +0100

    Fix confrence application population

--- src/data-sources/APPLICATION/CONFRENCE/controller.js
+++ src/data-sources/APPLICATION/CONFRENCE/controller.js
@@ -2,7 +2,7 @@ const confrenceApplication = require('./model');
 
 module.exports = {
 	getConfrenceApplications: async () => {
-		return await confrenceApplication.find().populate('applicant');
+		return await confrenceApplication.find().populate('applicant reviews.reviewer');
 	},
 
 	submitConfrenceApplication: async (input, context) => {
